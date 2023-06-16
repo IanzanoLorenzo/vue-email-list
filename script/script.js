20de7a3b27196b44686a28fa025818f5ac0d8407
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-16 13:34:03 +0200

    exercise done

--- script/script.js
+++ script/script.js
@@ -3,5 +3,21 @@
 const { createApp } = Vue
 
   createApp({
-
+    data() {
+      return {
+        mails:[],
+      }
+    },
+    mounted() {
+        this.createRandomMail();
+    },
+    methods: {
+        createRandomMail(){
+            for(let i = 0; i < 10;i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((newMail) => {
+                    this.mails.push(newMail.data.response);
+                })
+            }
+        }
+    },
 }).mount('#app')
